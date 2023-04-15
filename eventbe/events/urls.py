1a1a7946e182cf965ce6dad817a9dd515e18281a
reproductionprohibited <stantom16@gmail.com>
2023-04-15 21:20:28 +0300

    update: url_names

--- eventbe/events/urls.py
+++ eventbe/events/urls.py
@@ -7,8 +7,13 @@ app_name = 'events'
 urlpatterns = [
     path('', events.views.EventsListView.as_view(), name='events_list'),
     path(
-        'create_event/',
+        'create/',
         login_required(events.views.EventCreateView.as_view()),
         name='create_event',
+    ),
+    path(
+        'update/',
+        login_required(events.views.EventUpdateView.as_view()),
+        name='update_event',
     ),
 ]
