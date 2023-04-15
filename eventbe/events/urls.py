baff3ea11d7f5198dca59ce7c9835052bff6c1ea
reproductionprohibited <stantom16@gmail.com>
2023-04-15 17:08:15 +0300

    create: eventcreate form endpoint

--- eventbe/events/urls.py
+++ eventbe/events/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 import events.views
@@ -5,4 +6,9 @@ import events.views
 app_name = 'events'
 urlpatterns = [
     path('', events.views.EventsListView.as_view(), name='events_list'),
+    path(
+        'create_event/',
+        login_required(events.views.EventCreateView.as_view()),
+        name='create_event',
+    ),
 ]
