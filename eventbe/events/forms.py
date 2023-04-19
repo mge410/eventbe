d31d6044254aa6f3529eb479b98b94c1a439398e
reproductionprohibited <stantom16@gmail.com>
2023-04-19 20:51:46 +0300

    remove: event update

--- eventbe/events/forms.py
+++ eventbe/events/forms.py
@@ -29,39 +29,6 @@ class EventCreateForm(django.forms.ModelForm):
                 field.widget.attrs['readonly'] = 'readonly'
 
 
-class EventUpdateForm(django.forms.ModelForm):
-    class Meta:
-        model = events.models.Event
-        fields = [
-            events.models.Event.title.field.name,
-            events.models.Event.description.field.name,
-            events.models.Event.date.field.name,
-            events.models.Event.status.field.name,
-            events.models.Event.location_x.field.name,
-            events.models.Event.location_y.field.name,
-            events.models.Event.is_offline.field.name,
-            events.models.Event.tags.field.name,
-        ]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
-
-
-class EventThumbnailUpdateForm(django.forms.ModelForm):
-    class Meta:
-        model = events.models.EventThumbnail
-        fields = [
-            events.models.EventThumbnail.image.field.name,
-        ]
-
-        def __init__(self, *args, **kwargs) -> None:
-            super().__init__(*args, **kwargs)
-            for field in self.fields.values():
-                field.widget.attrs['class'] = 'form-control'
-
-
 class EventCommentForm(django.forms.ModelForm):
     class Meta:
         model = events.models.EventComment
