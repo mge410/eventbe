c64d057a9cb0695d0c130609c95c8fa3ee5dbd25
MrForquest <folp228@gmail.com>
2023-04-22 10:45:21 +0300

    Added openWeatherApi on detail event. Weather - now.

--- eventbe/static_dev/js/ya_map_detail_event.js
+++ eventbe/static_dev/js/ya_map_detail_event.js
@@ -42,4 +42,37 @@ function init() {
   myMap.setCenter(coords);
   myPlacemark.geometry.setCoordinates(coords);
   myMap.geoObjects.add(myPlacemark);
+
+  get_weather(coords);
+}
+
+function get_weather(coords) {
+  var apikey = "9fc94432db02a33b7772a666cd0d0f5f";
+  var url = "https://api.openweathermap.org/data/2.5/forecast";
+  var lang = document.getElementById("id_lang_code").innerHTML;
+  console.log(lang);
+  var res = $.get(
+    url,
+    (data = {
+      lat: coords[1],
+      lon: coords[0],
+      exclude: "daily",
+      APPID: apikey,
+      units: "metric",
+      lang: lang,
+      cnt: 16,
+      dt: Math.floor(Date.now() / 1000),
+    }),
+    function (data) {
+      var weather_container = document.getElementById("id_weather");
+      var temp_container = document.getElementById("id_temp");
+      var pressure_container = document.getElementById("id_pressure");
+      var humidity_container = document.getElementById("id_humidity");
+
+      weather_container.innerHTML = data.list[0].weather[0]["description"];
+      temp_container.innerHTML = data.list[0].main["temp"];
+      pressure_container.innerHTML = data.list[0].main["pressure"];
+      humidity_container.innerHTML = data.list[0].main["humidity"];
+    }
+  );
 }
